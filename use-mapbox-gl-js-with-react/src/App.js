7e3ae6b2b49608b75b72a54441686a8606a7a6c6
shabnomnom <shabnam.shomail@gmail.com>
2022-09-05 16:16:29 -0700

    hover working

--- use-mapbox-gl-js-with-react/src/App.js
+++ use-mapbox-gl-js-with-react/src/App.js
@@ -23,37 +23,121 @@ export default class App extends React.PureComponent {
       center: [lng, lat],
       zoom: zoom
     });
+    let hoveredCountyId = null;
   
 
     map.on('move', () => {
       this.setState({
         lng: map.getCenter().lng.toFixed(4),
         lat: map.getCenter().lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2)
+        zoom: map.getZoom().toFixed(2),
       });
     });
 
 		map.on('load', () => {
-			map.addSource('states', {
+			map.addSource('nbs', {
 			'type': 'geojson',
-			'data': NBData
-			});
-         
+			'data': NBData,
+      generateId: true,
+      })
+      map.addSource('ethnicity',{
+        type: 'vector',
+        url: 'mapbox://examples.8fgz4egr'
+      });
+
         map.addLayer({
-        'id': 'state-borders',
+        'id': 'NB-borders',
         'type': 'line',
-        'source': 'states',
+        'source': 'nbs',
         'layout': {},
 					'paint': {
 					'line-color': '#627BC1',
 					'line-width': 2
 					}
         });
+
+        map.addLayer({
+          'id': 'NB-fills',
+          'type': 'fill',
+          'source': 'nbs',
+          'layout': {},
+          'paint': {
+          'fill-color': '#627BC1',
+          'fill-opacity': [
+          'case',
+            ['boolean', ['feature-state', 'hover'], false],
+            1,
+            0.5
+            ]
+          }
+        });
+        map.addLayer({
+          'id': 'population',
+          'type': 'circle',
+          'source': 'ethnicity',
+          'source-layer': 'sf2010',
+          'paint': {
+          // Make circles larger as the user zooms from z12 to z22.
+          'circle-radius': {
+          'base': 1.75,
+          'stops': [
+          [12, 2],
+          [22, 180]
+          ]
+          },
+          // Color circles by ethnicity, using a `match` expression.
+          'circle-color': [
+          'match',
+          ['get', 'ethnicity'],
+          'White',
+          '#fbb03b',
+          'Black',
+          '#223b53',
+          'Hispanic',
+          '#e55e5e',
+          'Asian',
+          '#3bb2d0',
+          /* other */ '#ccc'
+          ]
+          }
+
 		});
+    // Updating feature state per hover 
+    map.on('mousemove', 'NB-fills', (e)=> {
+      if (e.features.length > 0) {
+        if (hoveredCountyId !== null){
+          console.log(e.features)
+          map.setFeatureState(
+            {
+              source: 'nbs',
+              id: hoveredCountyId,
+            },{
+              hover:false
+            });
+        }
+        hoveredCountyId = 
+        e.features[0].id;
+        map.setFeatureState(
+          {source: 'nbs', id:hoveredCountyId,},{ hover:true}
+        );
+      }    
+    });
+  
+    map.on('mouseleave','NB-fills', ()=>
+      {
+        if (hoveredCountyId !== null) {
+          map.setFeatureState(
+            {source: 'nbs', id:hoveredCountyId,},{ hover:false}
+          );
+        }
+        hoveredCountyId = null;
+      });
+
   }
 
+
   render() {
-    const { lng, lat, zoom } = this.state;
+
     return (
       <div>
         <div ref={this.mapContainer} className="map-container"/>
